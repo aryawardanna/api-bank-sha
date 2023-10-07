edff5ca1a3e69868e0df8ea187c33a5333c2a79c
I Wayan Arya Wardana <aryamadridfc@gmail.com>
2023-10-07 08:05:40 +0800

    updated all models

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,10 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'username',
+        'profile_picture',
+        'ktp',
+        'is_verified',
     ];
 
     /**
